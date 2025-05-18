e9819339e97477db9fc41423bc698c2322aa0939
demo <kashyapahaskar@gmail.com>
2025-05-18 13:06:27 +0530

    feat: add scenes demonstrating animate, Rotate, Transform, and ReplacementTransform

--- main.py
+++ main.py
@@ -39,3 +39,34 @@ class AnimateSquareToCircle(Scene):
         self.play(square.animate.rotate(PI/4))
         self.play(Transform(square, circle))
         self.play(FadeOut(square))
+
+class DifferentRotations(Scene):
+    def construct(self):
+        left_square = Square(color=BLUE, fill_opacity=0.7).shift(2 * LEFT)
+        right_square = Square(color=GREEN, fill_opacity=0.7).shift(2 * RIGHT)
+        self.play(
+            left_square.animate.rotate(PI), Rotate(right_square, angle=PI), run_time=2
+        )
+        self.wait()
+
+class TwoTransforms(Scene):
+    def transform(self):
+        a = Circle()
+        b = Square()
+        c = Triangle()
+        self.play(Transform(a, b))
+        self.play(Transform(a, c))
+        self.play(FadeOut(a))
+
+    def replacement_transform(self):
+        a = Circle()
+        b = Square()
+        c = Triangle()
+        self.play(ReplacementTransform(a, b))
+        self.play(ReplacementTransform(b, c))
+        self.play(FadeOut(c))
+
+    def construct(self):
+        self.transform()
+        self.wait(0.5)  # wait for 0.5 seconds
+        self.replacement_transform()
